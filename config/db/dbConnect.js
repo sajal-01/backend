18bf0a629b49743a6526ff65db4aa3d5f1c4198f
biosphere <dev.biosphere@gmail.com>
2022-10-01 02:42:27 +0530

    fc

---
+++ config/db/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Db is Connected Successfully');
+  } catch (error) {
+    console.log(`Error ${error.message}`);
+  }
+};
+
+module.exports = dbConnect;
